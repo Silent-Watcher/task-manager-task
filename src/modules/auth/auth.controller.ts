242fa07f558c87d19234738c0bb56165a0a0d949
Silent-Watcher <alitabatabaee20@gmail.com>
2025-06-02 16:56:42 +0330

    refactor(auth/register): review and remove some comments

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -6,20 +6,7 @@ import type { IAuthService } from './auth.service';
 import { authService } from './auth.service';
 
 const createAuthController = (service: IAuthService) => ({
-	/**
-	 * Handles user registration (v1).
-	 *
-	 * - Receives user registration data from the request body.
-	 * - Calls the service to create a new user and generate authentication tokens.
-	 * - Sets a secure HTTP-only refresh token cookie.
-	 * - Sends back the access token and basic user information in the response.
-	 *
-	 * @param {Request} req - Express request object containing the user registration data.
-	 * @param {Response} res - Express response object used to send the access token and user info.
-	 * @param {NextFunction} next - Express next function for error handling.
-	 *
-	 * @returns {Promise<void>}
-	 */
+	//
 	async registerV1(
 		req: Request,
 		res: Response,
--- src/modules/auth/auth.routes.ts
+++ src/modules/auth/auth.routes.ts
@@ -5,19 +5,8 @@ import { authController } from './auth.controller';
 
 const authRouterV1 = Router();
 
-// Register Route
-/**
- * @route POST /register
- * @group Authentication - Endpoints for user authentication
- * @summary Registers a new user.
- * @description
- * - Blocks the request if the user is already authenticated (`blockIfAuthenticated` middleware).
- * - Validates the request body against `zCreateUserDto`.
- * - Calls the `registerV1` controller to create a new user and issue authentication tokens.
- * @access Public
- */
 authRouterV1.post(
-	'/register',
+	'/signup',
 	validateBody(zCreateUserDto),
 	authController.registerV1,
 );
